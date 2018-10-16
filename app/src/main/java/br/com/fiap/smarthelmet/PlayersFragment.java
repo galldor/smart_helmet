13057fd0239c40089292731fe360192f58ef6196
Matheus Martins <matheus.martins@youse.com.br>
2018-10-16 18:05:10 -0300

    WIP

--- app/src/main/java/br/com/fiap/smarthelmet/PlayersFragment.java
+++ app/src/main/java/br/com/fiap/smarthelmet/PlayersFragment.java
@@ -44,7 +44,7 @@ public class PlayersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team, container, false);
+        return inflater.inflate(R.layout.fragment_players, container, false);
     }
 
     public void onButtonPressed(Uri uri) {
